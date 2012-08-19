fedc7c50c328e6bf4396c0e7ee4da5e1693e4aa4
Jabi <Jabi@Laptop.home>
2012-08-20 01:39:26 +0200

    Zoom support

--- code/org.eclipse_icons.editor/src/org/eclipse_icons/editor/IconsEditorPart.java
+++ code/org.eclipse_icons.editor/src/org/eclipse_icons/editor/IconsEditorPart.java
@@ -8,6 +8,8 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
@@ -15,12 +17,16 @@ import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
@@ -33,22 +39,28 @@ import org.eclipse_icons.editor.utils.ui.UIUtils;
 
 /**
  * Eclipse Icons Editor
- * 
  * @author Jabier Martinez
  */
 public class IconsEditorPart extends EditorPart implements ISaveablePart {
-
+	
+	// Editor
 	public static final String ID = "org.eclipse_icons.editor.iconEditor";
 	private FileEditorInput input;
 	Canvas canvas = null;
-	int pixelLength = 20;
 	int iconHeight;
 	int iconWidth;
 	boolean modified = false;
 	ImageData imageData;
 	protected List<PixelItem> pixels = new ArrayList<PixelItem>();
 	boolean drawing = false;
-
+	
+	// Zoom
+	private static final int ZOOM_MAXIMUM = 50;
+	private static final int ZOOM_MINIMUM = 1;
+	private static final int ZOOM_INITIAL = 20;
+	private Scale zoomScale = null;
+	int pixelLength = ZOOM_INITIAL;
+	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		monitor.beginTask("Save", 1);
@@ -150,9 +162,74 @@ public class IconsEditorPart extends EditorPart implements ISaveablePart {
 	}
 
 	@Override
-	public void createPartControl(Composite parent) {
+	public void createPartControl(Composite parent_original) {
 
-		canvas = createCanvas(parent, new PaintListener() {
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		parent_original.setLayout(gridLayout);
+		
+		ToolBar toolBar = new ToolBar (parent_original, SWT.FLAT);
+		ToolItem erase = new ToolItem (toolBar, SWT.CHECK);
+		erase.setToolTipText("Erase");
+		erase.setSelection(true);
+		erase.setImage(Activator.getImageDescriptor("icons/editor/erase.png").createImage());
+		erase.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				((ToolItem)e.getSource()).setSelection(true);
+			}
+		});
+		
+		@SuppressWarnings("unused")
+		ToolItem separator = new ToolItem(toolBar, SWT.SEPARATOR);
+		
+		ToolItem zoomOriginal = new ToolItem (toolBar, SWT.PUSH);
+		zoomOriginal.setToolTipText("Original size");
+		zoomOriginal.setImage(Activator.getImageDescriptor("icons/editor/zoomOriginal.png").createImage());
+		zoomOriginal.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				applyZoom(1);
+			}
+		});
+		
+		ToolItem zoomIn = new ToolItem (toolBar, SWT.PUSH);
+		zoomIn.setToolTipText("Zoom In");
+		zoomIn.setImage(Activator.getImageDescriptor("icons/editor/zoomIn.png").createImage());
+		zoomIn.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				applyZoom(pixelLength + 10);
+			}
+		});
+		
+		ToolItem zoomOut = new ToolItem (toolBar, SWT.PUSH);
+		zoomOut.setToolTipText("Zoom Out");
+		zoomOut.setImage(Activator.getImageDescriptor("icons/editor/zoomOut.png").createImage());
+		zoomOut.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				applyZoom(pixelLength - 10);
+			}
+		});
+		
+		GridData gridData = new GridData(GridData.FILL, SWT.BEGINNING, true, false);
+		toolBar.setLayoutData(gridData);
+		
+		zoomScale = new Scale (parent_original, SWT.NONE);
+		zoomScale.setToolTipText("Zoom");
+		zoomScale.setMinimum(ZOOM_MINIMUM);
+		zoomScale.setMaximum(ZOOM_MAXIMUM);
+		zoomScale.setSelection(ZOOM_INITIAL);
+		zoomScale.setIncrement(1);
+		
+		zoomScale.addListener(SWT.Selection, new Listener() {
+		      public void handleEvent(Event event) {
+		        applyZoom(zoomScale.getSelection());
+		      }
+		    });
+		
+		
+		gridData = new GridData(SWT.RIGHT, SWT.BEGINNING, false, false);
+		zoomScale.setLayoutData(gridData);
+		
+		canvas = createCanvas(parent_original, new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				GC gc = e.gc;
 				gc.drawRectangle(0, 0, pixelLength * iconWidth, pixelLength
@@ -164,7 +241,9 @@ public class IconsEditorPart extends EditorPart implements ISaveablePart {
 			}
 		});
 		
-		canvas.setLayout(new FillLayout());
+		gridData = new GridData(GridData.FILL, GridData.FILL, true, true);
+		gridData.horizontalSpan = 2;
+		canvas.setLayoutData(gridData);
 
 		// Mouse listeners to draw
 		canvas.addListener(SWT.MouseDown, new Listener() {
@@ -188,11 +267,7 @@ public class IconsEditorPart extends EditorPart implements ISaveablePart {
 			}
 		});
 
-		// initialize pixel positions
-		updatePixelsPositions(pixelLength);
-
-		// draw
-		canvas.redraw();
+		applyZoom(ZOOM_INITIAL);
 	}
 
 	@Override
@@ -270,6 +345,24 @@ public class IconsEditorPart extends EditorPart implements ISaveablePart {
 				y += pixelLength_p;
 			}
 		}
+	}
+	
+	/**
+	 * Apply Zoom
+	 * @param zoomValue
+	 */
+	public void applyZoom(int zoomValue){
+		// zoomValue inside zoom boundaries
+		if (zoomValue<ZOOM_MINIMUM){
+			zoomValue = ZOOM_MINIMUM;
+		} else if (zoomValue>ZOOM_MAXIMUM){
+			zoomValue = ZOOM_MAXIMUM;
+		}
+		// update and redraw
+		pixelLength = zoomValue;
+		updatePixelsPositions(pixelLength);
+		zoomScale.setSelection(pixelLength);
+		canvas.redraw();
 	}
 	
 	@Override
